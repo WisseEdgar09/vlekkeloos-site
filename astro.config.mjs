e9e075851103e8bf8e9f7d261c877dd07406d895
WisseEdgar09 <W.E.Seunnenga@student.rug.nl>
2026-06-03 14:22:47 +0200

    config: update site URL to vlekkeloos-leeuwarden.nl

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://vlekkeloos.nl',
+  site: 'https://vlekkeloos-leeuwarden.nl',
   integrations: [tailwind({ applyBaseStyles: false })],
   i18n: {
     locales: ['nl', 'en'],
