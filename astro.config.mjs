1ff0a9506388a1302386fc3708d60334d2caa3a5
WisseEdgar09 <W.E.Seunnenga@student.rug.nl>
2026-06-03 14:25:39 +0200

    feat: add @astrojs/sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://vlekkeloos-leeuwarden.nl',
-  integrations: [tailwind({ applyBaseStyles: false })],
+  integrations: [sitemap(), tailwind({ applyBaseStyles: false })],
   i18n: {
     locales: ['nl', 'en'],
     defaultLocale: 'nl',
